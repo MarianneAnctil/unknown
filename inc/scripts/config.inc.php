2d0662b1f241d838cc095b4158373b4015239f5a
Marianne Anctil <2163768@csfoy.ca>
2022-09-27 17:21:35 -0400

    ajout fichers

---
+++ inc/scripts/config.inc.php
@@ -0,0 +1,37 @@
+<?php
+ini_set('display_errors', 1);
+
+// Verifier si l'exécution se fait sur le serveur de développement (local) ou celui de la production:
+if (stristr($_SERVER['HTTP_HOST'], 'local') || (substr($_SERVER['HTTP_HOST'], 0, 7) == '192.168')) {
+    $blnLocal = TRUE;
+} else {
+    $blnLocal = FALSE;
+}
+
+// Selon l'environnement d'exécution (développement ou production)
+if ($blnLocal) {
+    $strHost = 'localhost';
+    $strBD='course_pwem2_2022_v2';
+    $strUser = 'course_pwem2_2022_v2';
+    $strPassword= 'MadameWatson1105';
+    error_reporting(E_ALL);
+} else {
+    $strHost = 'timunix2.cegep-ste-foy.qc.ca';
+    $strBD='prgm1_course';
+    $strUser = 'courseTim';
+    $strPassword = 'c0urset1m';
+    error_reporting(E_ALL & ~E_NOTICE);
+}
+
+//Data Source Name pour l'objet PDO
+$strDsn = 'mysql:dbname='.$strBD.';host='.$strHost;
+
+//Tentative de connexion
+$pdoConnexion = new PDO($strDsn, $strUser, $strPassword);
+//Changement d'encodage de l'ensemble des caractères pour UTF-8
+$pdoConnexion->exec("SET CHARACTER SET utf8");
+//Pour obtenir des rapports d'erreurs et d'exception avec errorInfo()
+$pdoConnexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//$pdoConnexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+
+?>
