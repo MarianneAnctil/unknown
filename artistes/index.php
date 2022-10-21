fe6028e10e2aec93354458c78eceb7f355ecb321
Marianne Anctil <2163768@csfoy.ca>
2022-10-21 13:18:44 -0400

    depart html artiste

---
+++ artistes/index.php
@@ -0,0 +1,24 @@
+<?php
+$niveau = '../'
+?>
+
+<!doctype html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="UTF-8">
+    <title>Liste_Artistes</title>
+    <?php include($niveau . "inc/fragments/header.inc.php") ?>
+</head>
+<body>
+<header>
+    <h1 class="h1"> ARTISTES </h1>
+</header>
+<main>
+
+</main>
+<footer>
+    <?php include($niveau . "inc/fragments/footer.inc.php") ?>
+</footer>
+</body>
