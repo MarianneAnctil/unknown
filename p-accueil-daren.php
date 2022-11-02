6b2ef89160e7a110a6e0989a7e83a420e57bdadd
darengarneaudionne <2065779@csfoy.ca>
2022-11-02 13:40:57 -0400

    Page accueil

--- p-accueil-daren.php
+++ p-accueil-daren.php
@@ -36,7 +36,7 @@ for($intCptEnr=0;$intCptEnr<$pdosResultatActualite->rowCount();$intCptEnr++){
     $Mois_texte = $arrMoisTexte[$ligne['Mois'] - 1];
     $arrArticle = explode(" ", $ligne['article']);
 
-    //Raccourcie le texte de l'actualite a 45 mots 
+    //Raccourcie le texte de l'actualite a 45 mots
     if (count($arrArticle)>45) {
         array_splice($arrArticle, 45, count($arrArticle));
     }
@@ -86,57 +86,75 @@ $arrArtisteAleatoire = shuffle($arrArtiste);
     <title>Festival OFF de Québec - Accueil</title>
 </head>
 <body>
-<header>
-    <nav class="nav">
-        <ul class="nav__list">
-            <li class="list__item"><a class="list__lien" href="#">Accueil</a></li>
-            <li class="list__item"><a class="list__lien" href="programmation/p-programmation-daren.php">Programmation</a></li>
-            <li class="list__item"><a class="list__lien" href="artistes/p-liste-daren.php">Artistes</a></li>
-            <li class="list__item"><a class="list__lien" href="#">Partenaires</a></li>
-        </ul>
-        <a class="lien-passeport" href="#">Acheter mon passeport</a>
-    </nav>
+<main>
     <div class="banniere-conteneur">
         <h1>Festival OFF de Québec</h1>
         <h2>Le 22 Novembre 2023</h2>
         <img class="banniere" src="https://via.placeholder.com/1920x800" alt="">
     </div>
-</header>
-<main>
     <section>
         <h3>Actualités</h3>
-        <?php
-            for($intCpt=0;$intCpt<$nombreArticleAfficher;$intCpt++){?>
         <section class="article">
             <article>
                 <header>
-                    <h4 class="article__titre"><?php echo $arrActualite[$intCpt]['titre'];?></h4>                
+                    <h4 class="article__titre"></h4>
                 </header>
-                <p class="article__description"><?php echo $arrActualite[$intCpt]['article'];?><a href="#">...</a></p>  
+                <p class="article__description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum dictum efficitur lacus et molestie. Maecenas consectetur velit quis purus sodales, ut accumsan ante molestie. Cras non maximus felis, nec dapibus nisi. Phasellus pulvinar ut neque vel venenatis. Vestibulum mattis faucibus dolor, a facilisis mauris fringilla et. Interdum et malesuada fames ac ante ipsum primis in faucibus.<a href="#"></a></p>
                 <footer>
-                    <time datetime="<?php echo $arrActualite[$intCpt]['date_actualite'];?>"><?php echo $arrActualite[$intCpt]['date'];?></time>
-                </footer>              
+                    <time datetime=""></time>
+                    <a href="">Lire la suite</a>
+                </footer>
             </article>
 
         </section>
-        <?php } ?>
     </section>
     <section>
         <h3>Artistes à découvrir</h3>
-        <?php
-        for($intCpt2=0;$intCpt2<$nombreArtisteAfficher;$intCpt2++){
-        ?>
-        <figure>
-            <img src="https://via.placeholder.com/250" alt="<?php echo $arrArtiste[$intCpt2]['nom_artiste'];?> en concert">
-            <figcaption><?php echo $arrArtiste[$intCpt2]['nom_artiste'];?></figcaption>
-            <a href="artistes/fiche/p-fiche-daren.php?id_artiste=<?php echo $arrArtiste[$intCpt2]['id_artiste'];?>">Lien vers l'artiste</a>
-        </figure>
-        <?php } ?>
+        <a href="">
+            <figure>
+                <figcaption>test</figcaption>
+                <img src="https://via.placeholder.com/400x400">
+            </figure>
+        </a>
+
     </section>
+    <section>
+        <h3>Lieux de spectacle</h3>
+        <a href="">Ouvrir dans google map
+            <img src="https://via.placeholder.com/400x400" alt="">
+        </a>
 
+    </section>
+    <section>
+        <h3>Tarif et lieux de vente</h3>
+        <div>
+            <div>
+                <h4>Passeport: 10$ pour toute la durée du festival</h4>
+                <ul>
+                    <li>5$ à la porte / soir (spectacles à Méduse)</li>
+                    <li>Spectacles extérieurs gratuits</li>
+                    <li>Spectacles gratuits au Parvis de l’église Saint-Jean-Baptiste, au bar le Sacrilège et au Fou-Bar</li>
+                </ul>
+                <p>Procurez-vous un passeport en ligne à <a href="https://lepointdevente.com/">lepointdevente.com</a> et profitez d’offres spéciales!</p>
+            </div>
+
+            <div>
+                <img src="https://via.placeholder.com/400x400"" alt="">
+            </div>
+        </div>
+        <div>
+            <h4>Les passeports sont aussi disponibles en prévente chez nos partenaires:</h4>
+            <ul>
+                <li>La Ninkasi Honoré-Mercier : 840 Avenue Honoré-Mercier, Québec</li>
+                <li>Érico: 634 Rue Saint-Jean, Québec</li>
+                <li>Le Sacrilège: 447 Rue Saint-Jean, Québec</li>
+                <li>Le Bonnet d’âne: 298 Rue Saint-Jean, Québec</li>
+                <li>Disquaire CD Mélomane : 248 rue Saint-Jean, Québec</li>
+                <li>Le Knock-Out: 832 St-Joseph Est, Québec</li>
+            </ul>
+        </div>
+    </section>
 </main>
-<footer>
-
-</footer>
 </body>
 </html>
