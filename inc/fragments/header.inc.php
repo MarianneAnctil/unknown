e7955eef8b1caae58d4b8208f47d669516b0514d
darengarneaudionne <2065779@csfoy.ca>
2022-11-09 15:05:37 -0500

    Ajout htmt et css

--- inc/fragments/header.inc.php
+++ inc/fragments/header.inc.php
@@ -4,7 +4,7 @@ $niveau="../../";
 <header>
     <a href="#contenu" class="visuallyhidden focusable">Allez au contenu</a>
     <nav class="nav" aria-label="Menu principal">
-        <a class="logo__lien" href=""><img class="logo__img" src="" alt="">LE OFF</a>
+        <a class="logo__lien" href="<?php echo $niveau ?>index.php"><img class="logo__img" src="" alt="">LE OFF</a>
         <ul class="nav__list" id="navList">
             <li class="nav__list-item"><a class="nav__list-lien" href="<?php echo $niveau ?>programmation/index.php">Programmation</a></li>
             <li class="nav__list-item"><a class="nav__list-lien" href="<?php echo $niveau ?>artistes/index.php">Artistes</a></li>
