725907b8479ba305999f58d926b52dc57dda83fa
darengarneaudionne <2065779@csfoy.ca>
2022-11-14 17:30:13 -0500

    Remise finale

--- inc/fragments/footer.inc.php
+++ inc/fragments/footer.inc.php
@@ -1,64 +1,54 @@
 
 <footer class="pied-de-page">
-    <hr>
-    <nav class="nav-secondaire">
-        <a href="">
-            <object type="images/svg+xml" data="images/logo.svg">
-                <!-- Pour les anciens navigateur -->
-                <img src="images/logo.svg" alt="Logo">
-            </object>
-        </a>
-        <ul class="nav-secondaire__list">
-            <li class="list__item"><a class="list__lien" href="<?php echo $niveau ?>programmation/index.php">Programmation</a></li>
-            <li class="list__item"><a class="list__lien" href="<?php echo $niveau ?>artistes/index.php">Artistes</a></li>
-            <li class="list__item"><a class="list__lien" href="#">Partenaires</a></li>
-        </ul>
-    </nav>
-    <div>
-        <div>
-            <h3>Contact</h3>
-            <a href="https://goo.gl/maps/xJFNDW8Z3yi4s7FV9">
+    <div class="footer-conteneur">
+        <section class="liste">
+            <nav class="nav-secondaire">
+                <a class="logo__lien" href="<?php echo $niveau ?>index.php">
+                    <img class="logo__img" src="<?php  echo $niveau ?>images/logos/logo_Off/SVG/off.svg" alt="logo">
+                </a>
+                <ul class="nav-secondaire__list">
+                    <li class="list__item"><a class="list__lien" href="<?php echo $niveau ?>programmation/index.php">Programmation</a></li>
+                    <li class="list__item"><a class="list__lien" href="<?php echo $niveau ?>artistes/index.php">Artistes</a></li>
+                    <li class="list__item"><a class="list__lien" href="#">Partenaires</a></li>
+                </ul>
+            </nav>                
+        </section>
+
+        <section class="contact">
+            <h4 class="footer__titre">Contact</h4>
+            <a class="lien__map" href="https://goo.gl/maps/xJFNDW8Z3yi4s7FV9">
                 110 boulvard René-Lévesque Ouest
                 C.P. 48036
                 Québec, Québec
                 G1R 5R5
             </a>
             <a href="mailto::info@quebecoff.org">info@quebecoff.org</a>
-            <a href="mailto::media@quebecoff.org">media@quebecoff.org</a>
-        </div>
-        <div>
-            <h3>Réseaux sociaux</h3>
+            <a href="mailto::media@quebecoff.org">media@quebecoff.org</a>                
+        </section>
+        <section class="reseaux">
+            <h4 class="footer__titre">Réseaux sociaux</h4>
             <div>
                 <a class="reseaux-sociaux__lien" href="#">
-                    <object type="image/svg+xml" data="image.svg">
-                        <!-- Pour les anciens navigateur -->
-                        <img src="images/instagram.svg" alt="Instagram">
-                    </object>
+                    <img src="images/medias-sociaux/instagram.svg" alt="Instagram">
                 </a>
                 <a class="reseaux-sociaux__lien" href="#">
-                    <object type="image/svg+xml" data="image.svg">
-                        <!-- Pour les anciens navigateur -->
-                        <img src="images/facebook.svg" alt="Facebook">
-                    </object>
+                    <img src="images/medias-sociaux/facebook.svg" alt="Facebook">
                 </a>
                 <a class="reseaux-sociaux__lien" href="#">
-                    <object type="image/svg+xml" data="image.svg">
-                        <!-- Pour les anciens navigateur -->
-                        <img src="images/twitter.svg" alt="Twitter">
-                    </object>
+                    <img src="images/medias-sociaux/twitter.svg" alt="Twitter">
                 </a>
                 <a class="reseaux-sociaux__lien" href="#">
-                    <object type="image/svg+xml" data="image.svg">
-                        <!-- Pour les anciens navigateur -->
-                        <img src="images/youtube.svg" alt="Youtube">
-                    </object>
+                    <img src="images/medias-sociaux/youtube.svg" alt="Youtube">
                 </a>
-            </div>
-            <form class="infolettre" action="">
+            </div>                    
+        </section>
+
+        <section class="infolettre">
+            <form class="infolettre__form" action="">
                 <label for="">Abonnez-vous à l'infolettre:</label>
                 <input type="email">
-            </form>
-        </div>
-        <small>© 2009-2022 Festival OFF Tous droits réservés</small>
-    </div>
+            </form>  
+        </section>
+    </div>    
+    <p class="pied">© 2009-2022 Festival OFF Tous droits réservés</p>
 </footer>
